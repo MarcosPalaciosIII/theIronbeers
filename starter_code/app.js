3cd59922a6a6f1085b3ca9db4558a5ebf718fc10
Marcos <mp2772@gmail.com>
2018-11-13 11:20:16 -0500

    updated comments

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,4 +1,4 @@
-
+// ====== variables and requires at the top ======
 const express = require('express');
 const hbs     = require('hbs');
 const app     = express();
@@ -6,13 +6,23 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+//  ===============================================
+
+
+
+
+// ================ MiddleWare ====================
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 hbs.registerPartials(__dirname + '/views/partials')
 app.use(express.static(path.join(__dirname, 'public')));
 
+//  ===============================================
 
 
+
+
+// =========== Routes at the bottom ==============
 
 app.get('/', (req, res, next) => {
   res.render('index');
@@ -35,12 +45,17 @@ app.get('/beersList', (req, res, next) => {
 app.get('/randomBeers', (req, res, next) => {
   punkAPI.getRandom()
   .then(theRandomBeer => {
-    console.log("---------- ", theRandomBeer)
+    // example on how to get 0 index in an array from route.
+    console.log("---------- ", theRandomBeer[0].method.mash_temp[0].temp.value)
+
+    // see randomBeer.hbs for how to get same index in view page.
     res.render('beerViews/randomBeer', {oneBeer: theRandomBeer})
   })
   .catch(error => {
     console.log(error)
   })
 })
+
+// ================================================
 
 app.listen(3000);
